162dee98c3fb83432e0ad8528a5e298333163b36
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-08-11 23:46:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+attr_accessor :roster, :name
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|hash, key| hash[key] = []}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  Hash[@roster.map { |key, value| [key, value.sort] }]
+end
+
+end
